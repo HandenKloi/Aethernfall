923c9ba4070e894425b87b60d5f127bf02b2a189
HandenKloi <7dbm44nbkz@privaterelay.appleid.com>
2026-09-01 14:45:01 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME='aethernfall-v33';
-const CORE=['./','./index.html','./style.css?v=3300','./game.js?v=3300','./manifest.json?v=3300'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(CORE).catch(()=>{})).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{const r=e.request;if(r.method!=='GET')return;const u=new URL(r.url);const app=u.pathname.endsWith('/')||/(\/index\.html|\/game\.js|\/style\.css|\/manifest\.json)$/.test(u.pathname);if(app){e.respondWith(fetch(r,{cache:'no-store'}).then(res=>{if(!res.ok)throw new Error('HTTP '+res.status);const c=res.clone();caches.open(CACHE_NAME).then(x=>x.put(r,c));return res}).catch(()=>caches.match(r)));return}if(/\.(png|jpg|jpeg|webp|svg|ico)$/i.test(u.pathname)){e.respondWith(caches.match(r).then(x=>x||fetch(r).then(res=>{if(!res.ok)throw new Error('HTTP '+res.status);const c=res.clone();caches.open(CACHE_NAME).then(x=>x.put(r,c));return res}).catch(()=>caches.match(r))));}});
