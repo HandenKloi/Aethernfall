d66524b00c93b0d7a54e72e922f6a708ebd61621
HandenKloi <7dbm44nbkz@privaterelay.appleid.com>
2026-09-01 13:08:09 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME='aethernfall-v32';
+const CORE=['./','./index.html','./style.css?v=3200','./game.js?v=3200','./manifest.json?v=3200'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(CORE).catch(()=>{})).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+ const r=e.request;if(r.method!=='GET')return;
+ const u=new URL(r.url);
+ const app=u.pathname.endsWith('/')||/(\/index\.html|\/game\.js|\/style\.css|\/manifest\.json)$/.test(u.pathname);
+ if(app){
+  e.respondWith(fetch(r,{cache:'no-store'}).then(res=>{
+   if(!res.ok)throw new Error('HTTP '+res.status);const copy=res.clone();caches.open(CACHE_NAME).then(c=>c.put(r,copy));return res
+  }).catch(()=>caches.match(r).then(x=>x||caches.match('./index.html'))));return;
+ }
+ if(/\.(png|jpg|jpeg|webp|svg|ico)$/i.test(u.pathname)){
+  e.respondWith(caches.match(r).then(x=>x||fetch(r).then(res=>{
+   if(!res.ok)throw new Error('HTTP '+res.status);const copy=res.clone();caches.open(CACHE_NAME).then(c=>c.put(r,copy));return res
+  })));
+ }
+});
